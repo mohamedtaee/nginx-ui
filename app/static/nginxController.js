83a2ac57e48f05a1b7584a083faff214903ba1c3
mohamedtaee <mohamed2@arizona.edu>
2024-05-26 00:30:04 -0500

    Added restart button and method to interact with container. Resolves #14.

---
+++ app/static/nginxController.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function () {
+    setNginxStatusCircle();
+});
+
+function setNginxStatusCircle() {
+    const nginxStatusCircle = document.getElementById('nginx-status-circle');
+
+    fetch('/api/status-nginx')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            const containerStatus = data.status;
+            console.log("Container status: " + containerStatus);
+            nginxStatusCircle.className = 'status-circle status-' + containerStatus;
+            return containerStatus;
+        });
+}
+
+let restartProcessComplete = true;
+
+function restartStatusInterval() {
+    const nginxStatusCircle = document.getElementById('nginx-status-circle');
+    nginxStatusCircle.className = 'status-circle status-loading';
+
+    const intervalId = setInterval(() => {
+        if (restartProcessComplete) {
+            clearInterval(intervalId);
+            setNginxStatusCircle();
+        } else {
+            if (nginxStatusCircle.className === 'status-circle status-loading') {
+                nginxStatusCircle.className = 'status-circle status-black';
+            } else {
+                nginxStatusCircle.className = 'status-circle status-loading';
+            }
+        }
+    }, 300);
+}
+
+
+function restartNginx() {
+    restartProcessComplete = false;
+    restartStatusInterval();
+
+    const restartBtn = document.getElementById('nginx-restart-btn');
+    restartBtn.disabled = true;
+
+    const restartSpinner = document.getElementById('nginx-restart-btn-spinner');
+    restartSpinner.className = 'custom-spinner custom-spinner-sm';
+
+
+    fetch('/api/restart-nginx', {method: "POST"})
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .finally(() => {
+            setNginxStatusCircle();
+            restartProcessComplete = true;
+            restartSpinner.className = '';
+            restartBtn.disabled = false;
+        });
+}
